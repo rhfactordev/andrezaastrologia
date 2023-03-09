5b8ab12ab8b89a28b7ea570b9f5c3b1028b2b1b7
Roberto Fonseca <roberto@rhfactor.com.br>
2023-03-09 00:53:15 -0300

    Formulário de Signos

--- components/EmailForm.vue
+++ components/EmailForm.vue
@@ -1,8 +1,25 @@
 <template>
   <form>
     <div class="form-group">
-      <input type="email" class="form-control" name="email" >
+      <input type="email" class="form-control" name="email" placeholder="email@domínio.com.br" >
     </div>
+    <div class="form-group">
+      <select class="form-control" name="sign" >
+        <option value="aries">Áries</option>
+        <option value="touro">Touro</option>
+        <option value="gemeos">Gêmeos</option>
+        <option value="cancer">Câncer</option>
+        <option value="leao">Leão</option>
+        <option value="virgem">Virgem</option>
+        <option value="libra">Libra</option>
+        <option value="escorpiao">Escorpião</option>
+        <option value="sagitario">Sagitário</option>
+        <option value="capricornio">Capricórnio</option>
+        <option value="aquario">Aquário</option>
+        <option value="peixes">Peixes</option>
+      </select>
+    </div>
+
     <button type="submit" class="btn btn-primary btn-lg btn-block" @click.stop.prevent="submit()">
       Quero me conhecer melhor!
     </button>
